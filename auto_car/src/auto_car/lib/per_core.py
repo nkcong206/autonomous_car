8619e9731ff242eba0483b493d32215667869ca6
Cong Nguyen Khac <nkcong206@gmail.com>
2023-12-05 22:27:04 +0700

    create_new_point root

--- auto_car/src/auto_car/lib/per_core.py
+++ auto_car/src/auto_car/lib/per_core.py
@@ -159,20 +159,20 @@ class Perception():
         initial_bearing = math.degrees(initial_bearing)
         return initial_bearing
         
-    def create_new_point (self, point, distance, bearing_degrees):
-        start_lat = math.radians(point[0])
-        start_lon = math.radians(point[1])    
+    def create_new_point (self, root, distance, bearing_degrees):
+        root_lat = math.radians(root[0])
+        root_lon = math.radians(root[1])    
 
         bearing_rad = math.radians(bearing_degrees)
 
         radius_earth_km = 6371.0 
         distance_km = distance/1000
         
-        end_lat = math.asin(math.sin(start_lat) * math.cos(distance_km / radius_earth_km) +
-                            math.cos(start_lat) * math.sin(distance_km / radius_earth_km) * math.cos(bearing_rad))
+        end_lat = math.asin(math.sin(root_lat) * math.cos(distance_km / radius_earth_km) +
+                            math.cos(root_lat) * math.sin(distance_km / radius_earth_km) * math.cos(bearing_rad))
 
-        end_lon = start_lon + math.atan2(math.sin(bearing_rad) * math.sin(distance_km / radius_earth_km) * math.cos(start_lat),
-                                        math.cos(distance_km / radius_earth_km) - math.sin(start_lat) * math.sin(end_lat))
+        end_lon = root_lon + math.atan2(math.sin(bearing_rad) * math.sin(distance_km / radius_earth_km) * math.cos(root_lat),
+                                        math.cos(distance_km / radius_earth_km) - math.sin(root_lat) * math.sin(end_lat))
 
         end_lat = math.degrees(end_lat)
         end_lon = math.degrees(end_lon)
