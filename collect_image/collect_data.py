20b31eb4c5464f255997be4f29261278860bce95
nkcong206 <nkcong206@gmail.com>
2024-01-18 14:54:29 +0900

    change color

--- collect_image/collect_data.py
+++ collect_image/collect_data.py
@@ -10,6 +10,7 @@ max_speed = 60
 
 speed = 0.0
 steering = 0.0
+capture = False
 route = 0 # 0 is straight, 1 is left, 2 is right
 action = 0 # 0 is straight, 1 is left, 2 is right
 junction = 0
@@ -22,66 +23,71 @@ class ps4controller(Controller):
         super().__init__('ps4controller')
         Controller.__init__(self, **kwargs)
     
-    def on_R1_press(self):
+    def on_left_arrow_press(self):
+        super().on_down_arrow_press
+        print("asdfdsfasdfasd")
+    
+    def on_L2_release(self):
         global capture
         with lock2:
-            capture = 1 
+            capture = True 
     
-    def on_L1_press(self):
+    def on_circle_press(self):
         global junction
         with lock2:
             junction = not junction 
+
+    def on_R2_release(self):
+        global action
+        with lock2:
+            action = 0
             
-    def on_square_press(self):
+    def on_L1_press(self):
+        super().on_L1_press
         global action
         with lock2:
             action = 1
     
-    def on_circle_press(self):
+    def on_R1_press(self):
         global action
         with lock2:
             action = 2
-    
-    def on_triangle_press(self):
-        global action
-        with lock2:
-            action = 0
-
-    def on_left_arrow_press(self):
-        global route
-        with lock2:
-            route = 1
-    
-    def on_right_arrow_press(self):
-        global route
-        with lock2:
-            route = 2
-    
-    def on_up_arrow_press(self):
+            
+    def on_square_press(self):
         global route
         with lock2:
             route = 0
     
-    def on_L3_y_at_rest(self):
+    def on_x_press(self):
+        global route
+        with lock2:
+            route = 1
+    
+    def on_triangle_press(self):
+        global route
+        with lock2:
+            route = 2
+    
+    def on_L3_release(self):
         global speed
         with lock1:
             speed = 0.0
 
-    def on_R3_x_at_rest(self):
-        global steering
-        with lock1:
-            steering = 0.0
-
     def on_L3_up(self, value):
         global speed
         with lock1:
-            speed = -value*50/32767
+            speed = -value/32767
 
     def on_L3_down(self, value):
         global speed
         with lock1:
-            speed = -value*50/32767
-
+            speed = -value/32767
+            
+    def on_R3_release(self):
+        global steering
+        with lock1:
+            steering = 0.0
+    
     def on_R3_left(self, value):
         global steering
         with lock1:
@@ -95,7 +101,7 @@ class ps4controller(Controller):
 def ps4_thread():
     while(True):
         try:   
-            ps4 = ps4controller(interface="/dev/input/js0").listen(timeout=5)
+            ps4 = ps4controller(interface="/dev/input/js0",  connecting_using_ds4drv=True).listen(timeout=5)
             time.sleep(10)
         except:
             print("disconnect")
@@ -104,62 +110,73 @@ def controller_thread():
     global speed, steering, capture, junction, route, action
     car = Pilot.AutoCar()
     car.setObstacleDistance(0)
-    car.camTilt(-5)
+    car.camTilt(-10)
     Util.enable_imshow()
     gstr = Util.gstrmer(width =640, height= 480, fps =30, flip =0)
     camera = cv2.VideoCapture(gstr, cv2.CAP_GSTREAMER)    
     max = check_file_number(path)
-    while(True):
-        car.steering = steering            
-        car.setSpeed(abs(speed*max_speed))
-        if speed > 0:
-            car.forward()
-        elif speed < 0:
-            car.backward()
-        else:
-            car.stop()
-        set_led_color(car,junction, route, action)
-        if (capture == 1):
-            ret, frame = camera.read()
-            car.alarm(scale=4, pitch = 8, duration = 0.3)
-            max = max + 1
-            cv2.imwrite(os.path.join(path , f'{max}_{route}_{junction}_{action}.jpg'), frame)
-            with lock2:
-                capture = 0
+    try:
+        while(True):
+            if speed < -99: speed = -99
+            if speed > 99: speed = 99
+            if steering < -1.0: steering = -1.0
+            if steering > 1.0: steering = 1.0
+            car.steering = steering            
+            car.setSpeed(abs(speed*max_speed))
+            if speed > 0:
+                car.forward()
+            elif speed < 0:
+                car.backward()
+            else:
+                car.stop()
+            set_led_color(car,junction, route, action)
+            print(junction, route, action)
+            if capture:
+                ret, frame = camera.read()
+                car.alarm(scale=4, pitch = 8, duration = 0.3)
+                max = max + 1
+                cv2.imwrite(os.path.join(path , f'{max}_{route}_{junction}_{action}.jpg'), frame)
+                with lock2:
+                    capture = False
+    except KeyboardInterrupt:
+        car.camTilt(0)
 
-def set_led_color(car, junction = -1, route = -1, action = -1):
-    if junction == 1:
+def set_led_color(car, capture = False, junction = -1, route = -1, action = -1):
+    if capture:
+        car.setPixelDisplay(2**3 + 2**4, [255,255,255])
+
+    if junction:
         car.setPixelDisplay(2**3 + 2**4, [255,0,0])
     
-    if route == 0:
-        car.setPixelDisplay(2**0 + 2**7, [0,255,0])
-    elif route == 1:
-        car.setPixelDisplay(2**0, [0,0,255])
+    if route == 1:
+        car.setPixelDisplay(2**2, [0,0,255])
     elif route == 2:
-        car.setPixelDisplay(2**7, [0,0,255])
+        car.setPixelDisplay(2**5, [0,0,255])
 
-    if action == 0:
-        car.setPixelDisplay(2**1 + 2**2 + 2**5 + 2**6, [255,255,255])
-    elif route == 1:
-        car.setPixelDisplay(2**1 + 2**2, [255,255,0])
-    elif route == 2:
-        car.setPixelDisplay(2**5 + 2**6, [255,255,0])    
+    if action == 1:
+        car.setPixelDisplay(2**1 + 2**0, [255,255,0])
+    elif action == 2:
+        car.setPixelDisplay(2**7 + 2**6, [255,255,0])    
         
     time.sleep(0.3)
     for i in range(8):
         car.setPixelDisplay(2**i, [0,0,0])
 
 def check_file_number(path):
-    file_list = os.listdir(path)
-    max_number = 0
-    for file in file_list:
-        number = int(file.split('_')[0])
-        if number > max_number:
-            max_number = number
-    return max_number
+    if not os.path.exists(path):
+        os.makedirs(path)
+        return 0
+    else:    
+        file_list = os.listdir(path)
+        max_number = 0
+        for file in file_list:
+            number = int(file.split('_')[0])
+            if number > max_number:
+                max_number = number
+        return max_number
 
 
-def main(args=None):
+def main():
     t1 = threading.Thread(target=ps4_thread)
     t2 = threading.Thread(target=controller_thread)
     t1.start()
